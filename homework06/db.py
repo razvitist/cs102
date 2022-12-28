e3a2b10b824a38a8e5364e889fbea1e3360af21b
Andrei Belomyttsev <ultrandrei@gmail.com>
2022-12-28 23:35:40 +0300

    x

--- homework06/db.py
+++ homework06/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer,  create_engine
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
